7c9fc620df5e3020315802486b5c8131208bb782
S. Panfilov <pnf036@gmail.com>
2026-01-31 02:15:09 +0100

    [REWORK] removed ts anarchy- path

--- vite.alias.ts
+++ vite.alias.ts
@@ -2,12 +2,8 @@ import path from 'node:path';
 
 export const sharedAliases = {
   '@Showcases/E2E': path.resolve(__dirname, 'apps/e2e/src'),
-  '@Anarchy/Engine': path.resolve(__dirname, 'packages/anarchy-engine/src'),
   '@Showcases/Menu': path.resolve(__dirname, 'packages/menu/src'),
   '@Showcases/GUI': path.resolve(__dirname, 'packages/gui/src'),
-  '@Anarchy/Shared': path.resolve(__dirname, 'packages/anarchy-shared/src'),
-  '@Anarchy/Tracking': path.resolve(__dirname, 'packages/anarchy-tracking/src'),
-  '@Anarchy/i18n': path.resolve(__dirname, 'packages/anarchy-i18n/src'),
   '@Showcases/i18n': path.resolve(__dirname, 'packages/i18n/src'),
   lodash: 'lodash-es'
 };
